60014b2e7f7f8d5c2f0dfeee70856780caf4109f
Guilherme Evangelista <guiga.equeirozan@gmail.com>
2021-03-19 21:45:24 -0400

    setup: jwt and laravel-responder

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -6,8 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
